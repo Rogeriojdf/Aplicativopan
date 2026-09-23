b2d4d47c9bdf671d61dfbfcd21f9a75b62153868
pcetec <pc@etec.com>
2026-09-23 20:48:23 -0300

    Exercicio 1

--- app/src/main/java/com/example/aplicativopan/MainActivity.kt
+++ app/src/main/java/com/example/aplicativopan/MainActivity.kt
@@ -13,44 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.aulaid)
+        setContentView(R.layout.exercico_nota)
 
-        val notaP1 = findViewById<EditText>(R.id.editNota1)
-        val notaP2 = findViewById<EditText>(R.id.editNota2)
-        val resultado = findViewById<TextView>(R.id.txtResultado)
-        val botao = findViewById<Button>(R.id.btnCalcular)
+        val botaoExerc1 = findViewById<Button>(R.id.btnExerc1)
+        val edtExerc1 = findViewById<EditText>(R.id.edtExec1)
+        val txtExerc1 = findViewById<TextView>(R.id.txtExerc1)
 
-        botao.setOnClickListener {
-            resultado.text = "Media é: " + ((notaP1.text.toString().toFloat()
-                    + notaP2.text.toString().toFloat()) / 2).toString()
-
+        botaoExerc1.setOnClickListener {
+            var numero = edtExerc1.text.toString().toFloat() * 2
+            txtExerc1.text = numero.toString()
         }
-
-        val peso = findViewById<EditText>(R.id.editPeso)
-        val altura = findViewById<EditText>(R.id.editAltura)
-        val resultadoimc = findViewById<TextView>(R.id.txtResultadoImc)
-        val botaoimc = findViewById<Button>(R.id.btnImc)
-
-        botaoimc.setOnClickListener {
-            val multiplica = (altura.text.toString().toFloat() * altura.text.toString().toFloat())
-            val imc = (peso.text.toString().toFloat()) / multiplica
-            resultadoimc.text = "IMC é: " + imc.toString()
-
-            if (imc <18.5)
-            {
-                resultadoimc.text = imc.toString() + " Abaixo do peso"
-            }else if(imc >= 18.5 && imc <= 24.9) {
-                resultadoimc.text = imc.toString() + " Peso Normal"
-            }
-            else if(imc >= 25 && imc <= 29.9) {
-                resultadoimc.text = imc.toString() + " Sobrepeso"
-            }else {
-                resultadoimc.text = imc.toString() + " Obesidade"
-            }
-
-
-        }
-
 
 
     }
